668e6e17db9e1734570cfc8754798a7ffa1e9883
Ahmed Farooki <ahmedfarooki@gmail.com>
2013-02-11 03:08:45 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Damnapp::Application.config.secret_token = 'd3ade10c3fc52f6f7a45d3127fb763214f255148e6f319765d7369cb8511ceb153cac111e9648271e6955d6f825a2d51c1af04b6c79a8e871f41ce2d56e7c9c4'
